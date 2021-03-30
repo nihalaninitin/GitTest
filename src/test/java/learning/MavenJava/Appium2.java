b0891070bdf17779586f36925ac2be5ae8cc2147
Nitin <nihalani.nitin@gmail.com>
2021-03-30 17:01:46 +0530

    commit from 2nd dev1

---
+++ src/test/java/learning/MavenJava/Appium2.java
@@ -0,0 +1,26 @@
+package learning.MavenJava;
+
+import org.testng.annotations.Test;
+
+public class Appium2 {
+
+	@Test
+	public void fbtest()
+	{
+		System.out.println("fbtest");
+		System.out.println("This is git test");
+	}
+
+	@Test
+	public void tweettest()
+	{
+		System.out.println("tweetest");
+		System.out.println("this is commit from git test master");
+	}
+	@Test
+	public void tweettest1()
+	{
+		System.out.println("tweetest");
+		System.out.println("this is commit from git test master");
+	}
+}
